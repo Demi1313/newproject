d3192f880caf614ecfe42316ba455b0a7d3fa5e7
Demi1313 <dmitrynikotin@mail.ru>
2023-02-03 19:31:07 +0300

    upd slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,12 @@
 
 $(document).ready(function(){
     $('.reviews__slider').slick({
+      arrows: true, 
+      dots: false, 
+      slidesToShow: 3, 
       centerMode: true,
-      centerPadding: '60px',
-      slidesToShow: 1,
-    });
+      centerPadding: '5px',
+  });
+
 });
 
