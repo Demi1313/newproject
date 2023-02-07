db84ec2b4e280d5e5ba45f73b90ee81be4a3f433
Demi1313 <dmitrynikotin@mail.ru>
2023-02-07 13:53:52 +0300

    final adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -6,8 +6,28 @@ $(document).ready(function(){
       slidesToShow: 3, 
       centerMode: true,
       centerPadding: '5px',
-      variableWidth: true
+      variableWidth: true,
+      responsive: [
+        {
+            breakpoint: 993,
+            settings: {
+                slidesToShow: 1,
+                variableWidth: false,
+                slidesToScroll: 1,
+            }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false, 
+            dots: true, 
+          }
+      }
+    ]
   });
-
+  $('.footer__map__close').on('click', function() {
+    $('.footer__map__leg').fadeOut('slow');
+  });
 });
+
 
